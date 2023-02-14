46bf2f92d0ff0df7b9c65a1b2931179fa513a49b
MesutRuzgar <mesutruzgar@gmail.com>
2023-02-14 21:11:20 +0300

    Experience codes tested and  added

--- KnightOnlineExpAsistant/Form1.Designer.cs
+++ KnightOnlineExpAsistant/Form1.Designer.cs
@@ -28,13 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxGerekenToplamExp = new System.Windows.Forms.TextBox();
+            this.tbxGelenExp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxDakikadaKesilenMob = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbx24SaatlikTotalExp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxSaatlikExpYuzde = new System.Windows.Forms.TextBox();
+            this.tbx24SaatlikExpYuzde = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GEREKEN TOPLAM EXP:";
+            // 
+            // tbxGerekenToplamExp
+            // 
+            this.tbxGerekenToplamExp.Location = new System.Drawing.Point(311, 39);
+            this.tbxGerekenToplamExp.Name = "tbxGerekenToplamExp";
+            this.tbxGerekenToplamExp.Size = new System.Drawing.Size(152, 26);
+            this.tbxGerekenToplamExp.TabIndex = 1;
+            // 
+            // tbxGelenExp
+            // 
+            this.tbxGelenExp.Location = new System.Drawing.Point(311, 71);
+            this.tbxGelenExp.Name = "tbxGelenExp";
+            this.tbxGelenExp.Size = new System.Drawing.Size(152, 26);
+            this.tbxGelenExp.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GELEN EXP:";
+            // 
+            // tbxDakikadaKesilenMob
+            // 
+            this.tbxDakikadaKesilenMob.Location = new System.Drawing.Point(311, 103);
+            this.tbxDakikadaKesilenMob.Name = "tbxDakikadaKesilenMob";
+            this.tbxDakikadaKesilenMob.Size = new System.Drawing.Size(152, 26);
+            this.tbxDakikadaKesilenMob.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DAKİKADA KESİLEN MOB SAYISI:";
+            // 
+            // tbx24SaatlikTotalExp
+            // 
+            this.tbx24SaatlikTotalExp.Enabled = false;
+            this.tbx24SaatlikTotalExp.Location = new System.Drawing.Point(311, 135);
+            this.tbx24SaatlikTotalExp.Name = "tbx24SaatlikTotalExp";
+            this.tbx24SaatlikTotalExp.Size = new System.Drawing.Size(152, 26);
+            this.tbx24SaatlikTotalExp.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(277, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "24 SAATTE GELECEK TOPLAM EXP";
+            // 
+            // tbxSaatlikExpYuzde
+            // 
+            this.tbxSaatlikExpYuzde.Enabled = false;
+            this.tbxSaatlikExpYuzde.Location = new System.Drawing.Point(311, 167);
+            this.tbxSaatlikExpYuzde.Name = "tbxSaatlikExpYuzde";
+            this.tbxSaatlikExpYuzde.Size = new System.Drawing.Size(152, 26);
+            this.tbxSaatlikExpYuzde.TabIndex = 5;
+            // 
+            // tbx24SaatlikExpYuzde
+            // 
+            this.tbx24SaatlikExpYuzde.Enabled = false;
+            this.tbx24SaatlikExpYuzde.Location = new System.Drawing.Point(311, 199);
+            this.tbx24SaatlikExpYuzde.Name = "tbx24SaatlikExpYuzde";
+            this.tbx24SaatlikExpYuzde.Size = new System.Drawing.Size(152, 26);
+            this.tbx24SaatlikExpYuzde.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(271, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SAATLİK GELECEK EXP YÜZDESİ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(289, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "24 SAATTE GELECEK EXP YÜZDESİ:";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(311, 232);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(152, 31);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(500, 294);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbx24SaatlikExpYuzde);
+            this.Controls.Add(this.tbxSaatlikExpYuzde);
+            this.Controls.Add(this.tbx24SaatlikTotalExp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxDakikadaKesilenMob);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxGelenExp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxGerekenToplamExp);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Knight Online Experience Assistant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxGerekenToplamExp;
+        private System.Windows.Forms.TextBox tbxGelenExp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxDakikadaKesilenMob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbx24SaatlikTotalExp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxSaatlikExpYuzde;
+        private System.Windows.Forms.TextBox tbx24SaatlikExpYuzde;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnHesapla;
     }
 }
 
--- KnightOnlineExpAsistant/Form1.cs
+++ KnightOnlineExpAsistant/Form1.cs
@@ -16,5 +16,25 @@ namespace KnightOnlineExpAsistant
         {
             InitializeComponent();
         }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double gunlukToplamExp, saatlikExpYuzde, gunlukExpYuzde, gelenExp, kesilenMob, gerekenExp;
+
+            gerekenExp = Convert.ToDouble(tbxGerekenToplamExp.Text);
+            gelenExp = Convert.ToDouble(tbxGelenExp.Text);
+            kesilenMob = Convert.ToDouble(tbxDakikadaKesilenMob.Text);
+
+            gunlukToplamExp = kesilenMob * 1440 * gelenExp;
+            tbx24SaatlikTotalExp.Text=gunlukToplamExp.ToString("0,000");
+
+            saatlikExpYuzde = (gunlukToplamExp/24)*100 / gerekenExp;
+            tbxSaatlikExpYuzde.Text = saatlikExpYuzde.ToString("00.##") + " %";
+
+            gunlukExpYuzde = gunlukToplamExp * 100 / gerekenExp;
+            tbx24SaatlikExpYuzde.Text = gunlukExpYuzde.ToString("00.##") + " %";
+
+
+        }
     }
 }
