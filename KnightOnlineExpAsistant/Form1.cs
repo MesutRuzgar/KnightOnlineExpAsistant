e2fa4d88b7598e8ae893ca3400e1931b3d08d1fe
MesutRuzgar <mesutruzgar@gmail.com>
2023-03-05 12:19:27 +0300

    kod iyilestirilmesi yapildi

--- KnightOnlineExpAsistant/Form1.cs
+++ KnightOnlineExpAsistant/Form1.cs
@@ -19,12 +19,12 @@ namespace KnightOnlineExpAsistant
 
         private void btnHesapla_Click(object sender, EventArgs e)
         {
-            if (tbxGerekenToplamExp.Text == "" && tbxGelenExp.Text == "" && tbxDakikadaKesilenMob.Text == "")
+            if (string.IsNullOrEmpty(tbxGerekenToplamExp.Text) && string.IsNullOrEmpty(tbxGelenExp.Text) && string.IsNullOrEmpty(tbxDakikadaKesilenMob.Text))
             {
-                MessageBox.Show("Lütfen Gerekli Alanları Doldurunuz.","Hata",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                MessageBox.Show("Lütfen Gerekli Alanları Doldurunuz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 tbxGerekenToplamExp.Focus();
             }
-            else if (tbxGerekenToplamExp.Text == "")
+            else if (string.IsNullOrEmpty(tbxGerekenToplamExp.Text))
             {
                 MessageBox.Show("Lütfen Gereken Toplam Exp Alanını Doldurunuz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 tbxGerekenToplamExp.Focus();
@@ -39,11 +39,37 @@ namespace KnightOnlineExpAsistant
                 MessageBox.Show("Lütfen Dakikada Kesilen Mob Alanını Doldurunuz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 tbxDakikadaKesilenMob.Focus();
             }
+            else if (!Int64.TryParse(tbxGerekenToplamExp.Text, out long result))
+            {
+
+                if (tbxGerekenToplamExp.Text.Length>18)
+                {
+                    MessageBox.Show("Çok fazla rakam yazıldı!", "Hata", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen giriniz!", "Hata", MessageBoxButtons.OK);
+                 
+                }
+                tbxGerekenToplamExp.Focus();
+            }
+            else if (!Int32.TryParse(tbxGelenExp.Text, out int result2))
+            {
+               
+                MessageBox.Show("Lütfen sadece sayı giriniz!", "Hata", MessageBoxButtons.OK);
+                tbxGelenExp.Focus();
+            }
+            else if (!double.TryParse(tbxDakikadaKesilenMob.Text, out double result3))
+            {
+
+                MessageBox.Show("Lütfen sadece ondalıklı sayı giriniz!", "Hata", MessageBoxButtons.OK);
+                tbxDakikadaKesilenMob.Focus();
+            }
             else
             {
                 ExpCalc();
             }
-            
+
 
         }
 
@@ -67,17 +93,18 @@ namespace KnightOnlineExpAsistant
 
         private void tbxGerekenToplamExp_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            //e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
         private void tbxGelenExp_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            //e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
         private void tbxDakikadaKesilenMob_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
+            //sadece harf ve virgül girebilir
+            //e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
         }
     }
 }
